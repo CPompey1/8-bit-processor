2c61d5a1a306a114a727b510d6890e9fd2decd83
Kyle A Lemma <kylelemm@timberlake.cse.buffalo.edu>
2023-03-29 15:53:02 -0400

    Added PC and Multiplexor

---
+++ CSE490-Project1.srcs/sources_1/new/program_counter.v
@@ -0,0 +1,34 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 03/29/2023 02:46:41 PM
+// Design Name: 
+// Module Name: program_counter
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module program_counter(
+    input [7:0] instructionAddress,
+    output [7:0] currentInstructionAddress
+    );
+    
+  reg [7:0] currentInstructionAddress;
+  
+  always @(instructionAddress)
+    begin
+      	assign currentInstructionAddress = instructionAddress;
+    end
+endmodule
